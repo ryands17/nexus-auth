8ae907dc5567d2d57b476612e83459a5bcac25f3
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-06-05 01:41:13 +0530

    add required env variables

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,24 +1,19 @@
 import { shield, allow } from 'graphql-shield'
 import { rules } from './rules'
 
-export const permissions = shield(
-  {
-    Query: {
-      me: rules.isAuthenticatedUser,
-      filterPosts: rules.isAuthenticatedUser,
-      post: rules.isAuthenticatedUser,
-      '*': allow,
-    },
-    Mutation: {
-      createDraft: rules.isAuthenticatedUser,
-      deletePost: rules.isPostOwner,
-      publish: rules.isPostOwner,
-    },
-    Subscription: {
-      latestPost: rules.isAuthenticatedUser,
-    },
+export const permissions = shield({
+  Query: {
+    me: rules.isAuthenticatedUser,
+    filterPosts: rules.isAuthenticatedUser,
+    post: rules.isAuthenticatedUser,
+    '*': allow,
   },
-  {
-    allowExternalErrors: true,
-  }
-)
+  Mutation: {
+    createDraft: rules.isAuthenticatedUser,
+    deletePost: rules.isPostOwner,
+    publish: rules.isPostOwner,
+  },
+  Subscription: {
+    latestPost: rules.isAuthenticatedUser,
+  },
+})
